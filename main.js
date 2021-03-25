1096a3efb2f8a09876093783a83ce721311bf3f6
ThangLianSuum <75762682+ThangLianSuum@users.noreply.github.com>
2021-03-24 18:16:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function draw() {
+    random = (1,2,3,4,5);
+    if(random = 1)
+    {
+        triangle(15, 90, 56, 0, 95, 68);
+        triangle(28, 75, 67, 37, 86, 89);
+        triangle(36, 67, 28, 0, 72, 56);
+ellipse(56, 46, 55, 55)
+circle(54, 36, 15)
+circle(68, 45, 30)
+};
+  
+
+} 
+
